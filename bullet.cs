f4f2733d57f60ef0bc41bf88d655f5b517d0e773
Mnoonan98 <45733955+Mnoonan98@users.noreply.github.com>
2019-12-13 14:51:48 +0000

    Add files via upload

---
+++ bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds;
+
+   
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(hitInfo.gameObject);
+    }
+
+    void OnColliderEnter2D(Collision2D hitInfo)
+    {
+        Debug.Log(hitInfo.gameObject.name); 
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > screenBounds.x * -2)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+   
+}
---
+++ ship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("hit detected");
+        Destroy(other.gameObject);
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hit detected");
+        Destroy(collision.gameObject);
+        this.gameObject.SetActive(false);
+    }
+}
